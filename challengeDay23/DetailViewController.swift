2265efbc87f472800d646f892f70bb6ae81a0712
kabikira <kabikirar2@protonmail.com>
2023-01-17 00:14:23 +0900

    first commit

---
+++ challengeDay23/DetailViewController.swift
@@ -0,0 +1,49 @@
+//
+//  DetailViewController.swift
+//  challengeDay23
+//
+//  Created by koala panda on 2023/01/16.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+        
+        }
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+
+        }
+        let item: [Any] = [image, selectedImage ?? "No name"]
+
+        let vc = UIActivityViewController(activityItems: item, applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- challengeDay23/ViewController.swift
+++ challengeDay23/ViewController.swift
@@ -7,11 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var flags = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "National Flags"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasSuffix("png") {
+                flags.append(item)
+            }
+        }
+        print(flags)
+    }
+    // テーブルに表示される行数を設定するメソッド
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flags.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Flags", for: indexPath)
+        cell.textLabel?.text = flags[indexPath.row]
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = flags[indexPath.row]
+            
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 
 
